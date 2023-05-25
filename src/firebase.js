235ff1e4478c8dd836166ce88c5b744aa7b68d39
newstarellie <newstarellie@gmail.com>
2023-05-25 14:51:20 +0800

    change firebaseConfig

--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD81zKsJk1cPMyw2HUYnyQnTZ8lS6WgrrE",
-  authDomain: "vueshop-955e1.firebaseapp.com",
-  databaseURL: "https://vueshop-955e1-default-rtdb.firebaseio.com",
-  projectId: "vueshop-955e1",
-  storageBucket: "vueshop-955e1.appspot.com",
-  messagingSenderId: "833229035584",
-  appId: "1:833229035584:web:2a2f325e7a2787d81da7a5",
-  measurementId: "G-GCE28MKY7Q"
+  apiKey: "AIzaSyAxy6tfCvlS2nJvLo51Xq_EadUEY63lwlw",
+  authDomain: "flashcard-f78b1.firebaseapp.com",
+  databaseURL: "https://flashcard-f78b1-default-rtdb.firebaseio.com",
+  projectId: "flashcard-f78b1",
+  storageBucket: "flashcard-f78b1.appspot.com",
+  messagingSenderId: "549913857702",
+  appId: "1:549913857702:web:cb39e7b32fc30f92b1ae67",
+  measurementId: "G-R9JGCCEFNF"
 };
 
 // Initialize Firebase
